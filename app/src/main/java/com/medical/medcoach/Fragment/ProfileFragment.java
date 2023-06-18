2ba25c1b0c719f38df4885a1953cad8ecbb7d439
Sonu Kumar <sonukumarmukhiya04@gmail.com>
2023-06-19 01:49:25 +0530

    Done!

--- app/src/main/java/com/medical/medcoach/Fragment/ProfileFragment.java
+++ app/src/main/java/com/medical/medcoach/Fragment/ProfileFragment.java
@@ -1,25 +1,68 @@
 package com.medical.medcoach.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.medical.medcoach.R;
+import com.medical.medcoach.getOTPActivity;
+
+import java.util.concurrent.TimeUnit;
 
 public class ProfileFragment extends Fragment {
+
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+
+    TextView UserName;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        UserName=view.findViewById(R.id.profile_name);
+
+        databaseReference= FirebaseDatabase.getInstance().getReference("Users");
+        databaseReference.child("+919262355173").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()){
+                    if (task.getResult().exists()){
+                        DataSnapshot dataSnapshot = task.getResult();
+                        String Name = String.valueOf(dataSnapshot.child("Full Name").getValue());
+                        UserName.setText(Name);
+
+                    }else {
+                        Toast.makeText(getActivity(), "User Not Exist", Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    Toast.makeText(getActivity(), "Failed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+     return view;
+    }
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
     }
