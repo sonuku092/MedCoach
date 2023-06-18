908145cdb945d006ea5b031a14d5f00bac5a035b
Sonu Kumar <sonukumarmukhiya04@gmail.com>
2023-06-19 02:15:22 +0530

    Done!

--- app/src/main/java/com/medical/medcoach/Fragment/BlogFragment.java
+++ app/src/main/java/com/medical/medcoach/Fragment/BlogFragment.java
@@ -7,16 +7,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.medical.medcoach.R;
 
 
 public class BlogFragment extends Fragment {
 
+    LinearLayout blog1;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blog, container, false);
+        View view= inflater.inflate(R.layout.fragment_blog, container, false);
+
+        blog1=view.findViewById(R.id.blog);
+        blog1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Yess Working", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
