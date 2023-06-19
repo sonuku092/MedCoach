4703afef56ecda5e9bfaf3d21070dfea860d4d6e
sajanadhikari <hariyoban0@gmail.com>
2023-06-19 08:45:12 +0530

    done

--- app/src/main/java/com/medical/medcoach/Fragment/CategoriesFragment.java
+++ app/src/main/java/com/medical/medcoach/Fragment/CategoriesFragment.java
@@ -1,6 +1,11 @@
 package com.medical.medcoach.Fragment;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.ImageView;
+
 
 import androidx.fragment.app.Fragment;
 
@@ -9,14 +14,28 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.medical.medcoach.R;
+import android.widget.ScrollView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 
 public class CategoriesFragment extends Fragment {
 
+    private ScrollView scrollView1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_categories, container, false);
+        scrollView1 = view.findViewById(R.id.scrollView);
+        RelativeLayout relativeLayout = view.findViewById(R.id.relativeLayout_ayurveda);
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Ayurveda", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_categories, container, false);
     }
 }
